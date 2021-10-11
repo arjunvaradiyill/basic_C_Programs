155059de1c1386954dbafad0d414c42067be8b85
alen4152 <75483634+alen4152@users.noreply.github.com>
2021-10-11 19:55:37 +0530

    Create comparealen.c

---
+++ comparealen.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter values of a and b \n");
+  scanf("%d",&a);
+  scanf("%d",&b);
+  
+  if(a>b)
+    printf("a is greater than b");
+  else if("b>a")
+    printf("b is greater than a");
+  else
+    printf("both numbers are same");
+  
+  return 0;
+  
+}
+  

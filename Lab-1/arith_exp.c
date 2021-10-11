4963f9091fea7c4b0b0956cd010ab13ca96891b4
Mathew Davis <mathewdavisme@gmail.com>
2021-10-11 22:53:06 +0530

    Trial

--- Lab-1/arith_exp.c
+++ Lab-1/arith_exp.c
@@ -2,7 +2,7 @@
 
 int main(){
 	int a,b,c,d,e,f,g;
-	printf("Enter the values\n");
+	printf("Enter the values: ");
 	scanf("%d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g);
 	float result=((a-b/c*d+e)*(f+g));
 	printf("The result is %f",result);
